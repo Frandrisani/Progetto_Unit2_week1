bfb8a976fc426fe4b33ba10e3186bd6b03ddd4cf
Francesco Andrisani <fra.andrisani01@gmail.com>
2023-12-22 16:09:01 +0100

    first commit

---
+++ base_di_partenza/script.js
@@ -0,0 +1,22 @@
+const navbar = function () {
+  const nav = document.querySelector("nav"); // Usa querySelector invece di getElementsByTagName
+  const button = document.getElementById("primoBottone");
+
+  window.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition < 300) {
+      nav.classList.add("navcolorye");
+      button.classList.add("getStarted");
+      nav.classList.remove("navcolorwh");
+      button.classList.remove("scroll");
+    } else {
+      nav.classList.remove("navcolorye");
+      button.classList.remove("getStarted");
+      nav.classList.add("navcolorwh");
+      button.classList.add("scroll");
+    }
+  });
+};
+
+navbar();
